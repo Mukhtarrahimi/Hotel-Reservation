083e4e0bd60a664f828cca306b2f4f15db3c6bfa
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-03-07 10:16:11 +0430

    backend: hotelController getHtel by id

--- backend/src/controllers/hotelController.js
+++ backend/src/controllers/hotelController.js
@@ -1,4 +1,5 @@
 import Hotel from "../models/hotelModel.js";
+import mongoose from "mongoose";
 import { uploadImage } from "../utils/uploadImage.js";
 import cloudinary from "../config/cloudinary.js";
 
@@ -81,6 +82,26 @@ export const getHotels = async (req, res) => {
 // Get Single Hotel
 export const getHotel = async (req, res) => {
   try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid hotel id",
+      });
+    }
+    const hotel = await Hotel.findById(id);
+    if (!hotel) {
+      return res.status(404).json({
+        success: false,
+        message: "Hotel not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "hotel fetched successfully",
+      data: hotel,
+    });
+
     res.json({ message: "get one hotel" });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
