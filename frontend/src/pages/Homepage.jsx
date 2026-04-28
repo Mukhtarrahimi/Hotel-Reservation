6ecb7b4c1a1b8712c635da73e68bb192b1d76e2d
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-04-28 10:29:23 +0430

    frontend: facility code desging

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import HotelList from "../components/HotelList";
+import Facility from '../components/Facility'
 
 function HomePage() {
   return (
     <div>
       <Hero />
       <HotelList />
+      <Facility/>
     </div>
   );
 }
