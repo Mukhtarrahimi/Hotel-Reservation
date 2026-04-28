ecd44c1f08b02eac9b2ad55023e9e4d2226a51bf
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-04-28 10:36:09 +0430

    frontend: footer file code

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      {/* Top Section */}
+      <div>
+        <h2>Sign Up For Exclusive Offers</h2>
+        <div>
+          <input type="email" placeholder="Enter your email address" />
+          <button>Join Now</button>
+        </div>
+      </div>
+      {/* Bottom Section */}
+      <div>
+        <div>
+          <h2>NEGAR HOTEL</h2>
+          <div>
+            <FaFacebook />
+            <FaInstagram />
+            <FaYoutube />
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li>HOME</li>
+            <li>BOOKINGS</li>
+            <li>ROOMS</li>
+            <li>ABOUT</li>
+            <li>CONTACTS</li>
+          </ul>
+        </div>
+      </div>
+      <p>@ 2026 NEGAR HOTELS. All rights reserved.</p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
