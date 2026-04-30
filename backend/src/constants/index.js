bccbc4c84a76bb2fc9932dba73b55b697e5feecc
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-04-30 14:03:02 +0430

    backend: constants/index.js

---
+++ backend/src/constants/index.js
@@ -0,0 +1,11 @@
+export const API = {
+  PREFIX: "/api",
+  VERSION: "v1",
+  BASE: "/api/v1",
+};
+
+export const ENV = {
+  DEVELOPMENT: "development",
+  PRODUCTION: "production",
+  TEST: "test",
+};
