9a5ea5e6477668978a2f99ece0565f8569186b45
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-05-02 14:34:01 +0430

    backend: server.js file update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,20 @@
 import dotenv from "dotenv/config";
-import  app  from "./app.js";
+import app from "./app.js";
+import { env } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(env.PORT, () => {
+      console.log(
+        `Server running on http://localhost:${PORT} in ${env.NODE_ENV} mode`,
+      );
+    });
+  } catch (error) {
+    console.error("Failed to start server: ", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
