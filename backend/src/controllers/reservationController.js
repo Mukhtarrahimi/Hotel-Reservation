10df1e027b2ed4364eaf4aee8be88499e9c8ab8c
Mukhtarrahimi <mukhtarrahimi.dev@gmail.com>
2026-05-08 21:34:42 +0430

    backend: reservationController.js

---
+++ backend/src/controllers/reservationController.js
@@ -0,0 +1,52 @@
+import Reservation from "../models/reservationModel.js";
+
+// Create Reservation
+export const createReservation = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+// Get All Reservation
+export const getReservations = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+// Get Reservation By ID
+export const getReservation = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+// Update Reservation
+export const updateReservation = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+// Delete Reservation
+export const deleteReservation = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
